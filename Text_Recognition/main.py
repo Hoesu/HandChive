7a0a19137841c3db69b459ad9e44d5524235c073
root <treecollector@yonsei.ac.kr>
2023-12-01 01:58:46 +0000

    UPDATE

--- Text_Recognition/main.py
+++ Text_Recognition/main.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
 from PIL import Image
+from rich.progress import Progress
 
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from datasets import load_metric
 
 from tqdm.notebook import tqdm
 import torchvision.models as models
@@ -44,6 +46,9 @@ def dataloader(image_dir, label_dir, processor):
     ## 데이터셋 클래스에 먹여주기
     train_dataset = HandWriting(image_dir=image_dir, df=train_df, processor=processor)
     test_dataset = HandWriting(image_dir=image_dir, df=test_df, processor=processor)
+    ## 스크립트 출력
+    print("Number of training examples:", len(train_dataset))
+    print("Number of validation examples:", len(test_dataset))
     ## 데이터 로더
     train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=10)
@@ -77,44 +82,92 @@ class HandWriting(Dataset):
         ## 변환한 이미지와 텍스트에 대한 인코딩을 딕셔너리에 저장. 모델 입력값으로 쓰일 예정.
         encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
         return encoding
+
+def compute_cer(pred_ids, label_ids):
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
+    label_str = processor.batch_decode(label_ids, skip_special_tokens=True)
+    cer_metric = load_metric("cer")
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+    return cer
+
+def train(model, train_dataloader, test_dataloader, device, num_epochs=1):
+    optimizer = AdamW(model.parameters(), lr=5e-5)
     
-from transformers import AdamW
-from tqdm.notebook import tqdm
-import torchvision.models as models
+    for epoch in range(num_epochs):
+        # Train
+        model.train()
+        train_loss = 0.0
+        with Progress() as progress:
+            task = progress.add_task("[cyan]Training...", total=len(train_dataloader))
+            for batch in train_dataloader:
+                # Move batch to device
+                for k, v in batch.items():
+                    batch[k] = v.to(device)
 
-optimizer = AdamW(model.parameters(), lr=5e-5)
+                # Forward + Backward + Optimize
+                outputs = model(**batch)
+                loss = outputs.loss
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
 
-for epoch in range(1):  # loop over the dataset multiple times
-   # train
-   model.train()
-   train_loss = 0.0
-   for batch in tqdm(train_dataloader):
-      # get the inputs
-      for k,v in batch.items():
-        batch[k] = v.to(device)
+                train_loss += loss.item()
+                progress.update(task, advance=1)
 
-      # forward + backward + optimize
-      outputs = model(**batch)
-      loss = outputs.loss
-      loss.backward()
-      optimizer.step()
-      optimizer.zero_grad()
+        print(f"Loss after epoch {epoch}:", train_loss / len(train_dataloader))
 
-      train_loss += loss.item()
+        # Evaluate
+        model.eval()
+        valid_cer = 0.0
+        with Progress() as progress:
+            task = progress.add_task("[green]Evaluating...", total=len(test_dataloader))
+            with torch.no_grad():
+                for batch in test_dataloader:
+                    # Run batch generation
+                    outputs = model.generate(batch["pixel_values"].to(device))
+                    # Compute metrics
+                    cer = compute_cer(pred_ids=outputs, label_ids=batch["labels"])
+                    valid_cer += cer
+                    progress.update(task, advance=1)
 
-   print(f"Loss after epoch {epoch}:", train_loss/len(train_dataloader))
-    
-   # evaluate
-   model.eval()
-   valid_cer = 0.0
-   with torch.no_grad():
-     for batch in tqdm(test_dataloader):
-       # run batch generation
-       outputs = model.generate(batch["pixel_values"].to(device))
-       # compute metrics
-       cer = compute_cer(pred_ids=outputs, label_ids=batch["labels"])
-       valid_cer += cer 
+        print("Validation CER:", valid_cer / len(test_dataloader))
 
-   print("Validation CER:", valid_cer / len(test_dataloader))
+    # Save the trained model
+    save_model(model, '/root/HandChive/Text_Recognition/model/trocr.pth')
 
-torch.save(model, '저장할 위치')
+def inference(input_dir, processor, model, device):
+    lines = os.listdir(input_dir)
+    generated_text = ""
+    for line in lines:
+        image_path = os.path.join(input_dir, line)
+        image = Image.open(image_path).convert('RGB')
+        pixel_values = processor(image, return_tensors='pt').pixel_values
+        pixel_values = pixel_values.to(device)
+        generated_ids = model.generate(pixel_values)
+        generated_text += processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(generated_text)
+
+
+processor = load_processors()
+#create_model('/root/HandChive/Text_Recognition/model/trocr.pth')
+model = load_model('/root/HandChive/Text_Recognition/model/trocr.pth')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# set special tokens used for creating the decoder_input_ids from the labels
+model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+# make sure vocab size is set correctly
+model.config.vocab_size = model.config.decoder.vocab_size
+
+# set beam search parameters
+model.config.eos_token_id = processor.tokenizer.sep_token_id
+model.config.max_length = 64
+model.config.early_stopping = True
+model.config.no_repeat_ngram_size = 3
+model.config.length_penalty = 2.0
+model.config.num_beams = 4
+
+#train_dataloader, test_dataloader = dataloader('/root/HandChive/Text_Recognition/data/crop_image','/root/HandChive/Text_Recognition/data/crop_label/crop_label.csv',processor)
+#train(model, train_dataloader, test_dataloader, device)
+inference('/root/HandChive/Text_Recognition/data/crop_image',processor,model,device)
